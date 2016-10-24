7e3d76e437d1b5af167a29cec7edd33710138d95
park ji yeon <jypark2781@gmail.com>
2016-10-24 18:04:06 +0900

    resume화면 레이아웃

--- www/js/main/main-controller.js
+++ www/js/main/main-controller.js
@@ -17,4 +17,16 @@ mainCtrl = function($scope, $state, $timeout) {
   }
 }
 
-app.controller('mainCtrl', mainCtrl);
+app.controller('mainCtrl', mainCtrl)
+.controller('MyCtrl', function($scope, $document){
+    $scope.toTheTop = function() {
+      $document.scrollTopAnimated(0, 5000).then(function() {
+        console && console.log('You just scrolled to the top!');
+      });
+    }
+    var section3 = angular.element(document.getElementById('section-3'));
+    $scope.toSection3 = function() {
+      $document.scrollToElementAnimated(section3);
+    }
+  }
+).value('duScrollOffset', 30);
---
+++ www/js/resume/resume-directive.js
@@ -0,0 +1,10 @@
+resumeDirective = function() {
+  return {
+    templateUrl: 'templates/resume/resume.html',
+    controller: function($scope) {
+
+    }
+  }
+}
+
+app.directive('resumeDirective', resumeDirective);
